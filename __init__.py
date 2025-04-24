b67d32aee2de84382bb5cb8530c8d0ac408099ca
yeeyou <corner52@gmail.com>
2025-04-24 14:05:18 +0800

    11

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# Import the mappings from your node file
+from .my_image_mask_utils import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# Optionally print a confirmation message (helps debugging)
+print("--- Loading nodes from comfyui-my-utils ---")
+
+# Make sure __all__ is defined if you want to be explicit (optional but good practice)
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
